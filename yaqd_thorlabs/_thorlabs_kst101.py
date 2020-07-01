b410c31647ad7e8a8d3288664006d8c62a8ddbe0
Kyle Sunden <git@ksunden.space>
2020-07-01 23:45:53 +0000

    Avro

--- yaqd_thorlabs/_thorlabs_kst101.py
+++ yaqd_thorlabs/_thorlabs_kst101.py
@@ -1,10 +1,5 @@
-from .__version__ import __branch__
 from ._thorlabs_apt_motor import ThorlabsAptMotor
 
 
 class ThorlabsKS101(ThorlabsAptMotor):
     _kind = "thorlabs-ks101"
-    _version = "0.1.0" + f"+{__branch__}" if __branch__ else ""
-    defaults = {
-        "units": "mm",
-    }
